27f3de487b517adedd3fa544bc18616e56b5fa8f
Paul0522 <paul.milcape@gmail.com>
2023-04-22 21:58:00 -0500

    Modificacion de la funcion sort_list y tambien se creo una funcion para traducir los mensajes

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
 
 
-def sort_list(items, ascending=True):
+def sort_list(items, ascending=True, deleteDuplicates=False):
+
     if not isinstance(items, list):
         raise RuntimeError(f"No puede ordenar {type(items)}")
+    
+    listaOrdenda = sorted(items, reverse=(not ascending))
 
-    return sorted(items, reverse=(not ascending))
+    if deleteDuplicates:
+        listaOrdenda = list(set(listaOrdenda))  # Elimina palabras duplicadas
+
+    return listaOrdenda #sorted(items, reverse=(not ascending))
 
 
 def remove_duplicates_from_list(items):
@@ -28,11 +34,12 @@ if __name__ == "__main__":
         filename = sys.argv[1]
         remove_duplicates = sys.argv[2].lower() == "yes"
     else:
-        print("Se debe indicar el fichero como primer argumento")
-        print("El segundo argumento indica si se quieren eliminar duplicados")
+        #print("Se debe indicar el fichero como primer argumento")
+        print(translateMessage("Se debe indicar el fichero como primer argumento"))
+        print(translateMessage("El segundo argumento indica si se quieren eliminar duplicados"))
         sys.exit(1)
-
-    print(f"Se leerán las palabras del fichero {filename}")
+    wordFile = translateMessage("Se leerán las palabras del fichero")
+    print(f"{wordFile} {filename}")
     file_path = os.path.join(".", filename)
     if os.path.isfile(file_path):
         word_list = []
@@ -40,10 +47,29 @@ if __name__ == "__main__":
             for line in file:
                 word_list.append(line.strip())
     else:
-        print(f"El fichero {filename} no existe")
+        file    = translateMessage("El fichero")
+        file1   = translateMessage("no existe" )
+        print(f"{file} {filename} {file1}")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
 
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
     print(sort_list(word_list))
+
+def translateMessage(message):
+    #Diccionario de traducciones
+    translations = {
+        "Se debe indicar el fichero como primer argumento":"The file must be specified as the first argument",
+        "El segundo argumento indica si se quieren eliminar duplicados":"The second argument indicates whether you want to remove duplicates",
+        "Se leerán las palabras del fichero":"The words of the file will be read",
+        "El fichero":"The file",
+        "no existe":"does not exist"
+    }
+
+    # Verificar si el mensaje está en el diccionario de traduccion
+    if message in translations:
+        return translations[message]
+    else:
+        #Si el mensaje no está en el diccionario
+        return message
